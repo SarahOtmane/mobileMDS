9fd7132ac1b61663fa8c5f954d3f71e1799d8ddb
SarahOtmane <sarahotmane02@gmail.com>
2024-07-06 00:01:17 +0200

    add button

--- components/Compte.js
+++ components/Compte.js
@@ -4,16 +4,17 @@ import Header from "./sections/Header";
 
 import compteStyle from "../styles/compteStyle";
 import Style from '../Style';
+import Button from "./sections/Button";
 
 
 const Compte = () =>{
     return(
         <ScrollView style={compteStyle.container}>
-            <View>
-                <Header />
-                <Image style={compteStyle.image} source={require('../assets/compte.png')} />
-                <Text style={[Style.h6, Style.m_R10, Style.m_T20]}>Identifiez vous pour suivre vos commandes, valider votre commande.</Text>
-            </View>
+            <Header />
+            <Image style={compteStyle.image} source={require('../assets/compte.png')} />
+            <Text style={[Style.h6, Style.m_R10, Style.m_T20]}>Identifiez vous pour suivre vos commandes, valider votre commande.</Text>
+
+            <Button text='Se connecter' arrow='false' />
         </ScrollView>
     )
 }
