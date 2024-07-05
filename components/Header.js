8a41787cb2c765b8865dc3f8fb643a81bc98ce2a
SarahOtmane <sarahotmane02@gmail.com>
2024-07-05 21:34:37 +0200

    composant header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import SvgUri from 'react-native-svg-uri';
 
-export default Header = () =>{
-    return(
-        <View>
-            <Image source={require('../assets/logo.svg')} />
-            <Image source={require('../assets/panier.svg')} />
-        </View>
-    )
+const Header = () => {
+  return (
+    <View style={styles.headerContainer}>
+      <SvgUri
+        width="150"
+        height="50"
+        source={require('../assets/logo.svg')}
+      />
+      <SvgUri
+        width="50"
+        height="50"
+        source={require('../assets/panier.svg')}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFDBE8',
+    width: '100%',
+    margin: 0,
+    padding: 0,
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    margin: 10,
+  },
+});
+
+export default Header;
