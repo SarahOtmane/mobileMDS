fb2d6c42dd8779d179030581b8fc9e355e20be42
SarahOtmane <sarahotmane02@gmail.com>
2024-07-05 23:50:06 +0200

    update

--- styles/accueilStyle.js
+++ styles/accueilStyle.js
@@ -1,22 +1,6 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#EFEFEF',
-    },
-    header: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingVertical: 40,
-        paddingRight: 20,
-        paddingTop: 100,
-    },
-
-
-
     reparation:{
         backgroundColor: '#FFDBE8',
         paddingLeft : 25,
@@ -39,29 +23,5 @@ export default StyleSheet.create({
     },
     tutoText:{
         marginVertical : 10,
-    },
-
-
-    m_B20:{
-        marginBottom: 20
-    },
-    m_B10:{
-        marginBottom: 10
-    },
-    h1:{
-        fontSize: 26,
-        width : 300,
-    },
-    h2:{
-        fontSize: 20,
-    },
-    h3:{
-        fontSize: 16,
-    },
-    h6:{
-        fontSize: 14,
-    },
-    p:{
-        fontSize: 12,
     },
 })
