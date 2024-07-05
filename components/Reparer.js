be7f54d24227f997b5c78f690f43c9c1ba7f4ffa
SarahOtmane <sarahotmane02@gmail.com>
2024-07-06 01:08:01 +0200

    progress bar valide

--- components/Reparer.js
+++ components/Reparer.js
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native"
+import React from 'react';
+import { ScrollView, View, Text } from "react-native";
 
+import compteStyle from "../styles/compteStyle"; 
+import repareStyle from "../styles/repareStyle"; 
 
-const Reparer = () =>{
-    return(
-        <View>
-            <Text>Sarah</Text>
-        </View>
-    )
+const Reparer = () => {
+    return (
+        <ScrollView style={compteStyle.container}>
+            <Header />
+            <View>
+                <Text style={repareStyle.progressText}>1/2 Votre Réparation</Text>
+                <View style={repareStyle.progressBar}>
+                    <View style={repareStyle.progress}></View>
+                </View>
+            </View>
+        </ScrollView>
+    );
 }
 
 export default Reparer;
