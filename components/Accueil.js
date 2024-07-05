bc881feefc97dc21ec56e1e96b8214498438f35f
SarahOtmane <sarahotmane02@gmail.com>
2024-07-05 23:53:39 +0200

    import du fichier Style

--- components/Accueil.js
+++ components/Accueil.js
@@ -3,21 +3,22 @@ import { View, Text, ScrollView } from 'react-native';
 import Svg, { G, Path, Defs, ClipPath, Rect } from 'react-native-svg';
 
 import accueilStyle from '../styles/accueilStyle';
+import Style from '../Style';
+
 import Etape from './sections/Etape';
 import Header from './sections/Header';
-import Video from './sections/Video';
 import Button from './sections/Button';
 
 const Accueil = () => {
     return (
         <ScrollView>
-            <View style={[accueilStyle.reparation, accueilStyle.m_B20]}>
+            <View style={[accueilStyle.reparation, Style.m_B20]}>
                 <Header />
 
-                <Text style={[accueilStyle.h1, accueilStyle.m_B20]}>
+                <Text style={[Style.h1, Style.m_B20]}>
                     Faites des affaires avec vos affaires
                 </Text>
-                <Text style={[accueilStyle.h3, accueilStyle.m_B20]}>
+                <Text style={[Style.h3, Style.m_B20]}>
                     Réparez vos vêtements pour leur offrir une seconde vie
                 </Text>
 
@@ -25,10 +26,10 @@ const Accueil = () => {
             </View>
 
             <View style={[accueilStyle.tuto]} >
-                <Text style={[accueilStyle.h6, accueilStyle.m_B20]}>
+                <Text style={[Style.h6, Style.m_B20]}>
                     Comment réparer avec RenoWear ?
                 </Text>
-                <Text style={[accueilStyle.h6]}>
+                <Text style={[Style.h6]}>
                     Pas besoin de nous suivre à l'autre bout du monde. Il vous suffit de suivre ce tuto . 
                 </Text>
 
