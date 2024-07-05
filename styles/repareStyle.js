2d7caa4e06df3919708d6960c11e9503a26e6081
SarahOtmane <sarahotmane02@gmail.com>
2024-07-06 01:08:07 +0200

    style progress bar

--- styles/repareStyle.js
+++ styles/repareStyle.js
@@ -1,5 +1,21 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-    
-})
+    progressBar: {
+        height: 10,
+        width: 350,
+        borderRadius: 5,
+        backgroundColor: 'F5F5F5',
+        marginVertical: 10, 
+    },
+    progress: {
+        height: 10,
+        borderRadius: 5,
+        width: 175,
+        backgroundColor: '#BDDEB4',
+    },
+    progressText: {
+        textAlign: 'right',
+        marginRight: 25
+    }
+});
