c18bd2e4550e258cc9c8799d7baa77eb4bc5b807
SarahOtmane <sarahotmane02@gmail.com>
2024-07-05 23:47:30 +0200

    style du composant compte

---
+++ styles/compteStyle.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#EFEFEF',
+        paddingLeft : 20
+    },
+
+    image: {
+        marginLeft: 70
+    }
+})
