a31625c61a62b147a2f4e00ff3098c73df9376e9
SarahOtmane <sarahotmane02@gmail.com>
2024-07-05 23:34:54 +0200

    update composant Button

--- components/sections/Button.js
+++ components/sections/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Svg, { G, Path, Defs, ClipPath, Rect } from 'react-native-svg';
 
 
-export default Button = ({text}) =>{
+export default Button = ({text, arrow}) =>{
     const handleSubmit = () => {
       };
 
@@ -11,7 +12,11 @@ export default Button = ({text}) =>{
             style={styles.button}
             onPress={handleSubmit}
         >
-          <Text style={styles.buttonText}>{text}</Text>
+            <Text style={styles.buttonText}>{text}</Text>
+            {arrow && <Svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <Path fillRule="evenodd" clipRule="evenodd" d="M18.2896 11.9958C18.5701 12.2743 18.5701 12.7257 18.2896 13.0042L12.7541 18.5L11.7384 17.4916L16.766 12.5L11.7384 7.50845L12.7541 6.5L18.2896 11.9958Z" fill="#999999"/>
+                <Path fillRule="evenodd" clipRule="evenodd" d="M17.9972 13.2131H5.5V11.7869H17.9972V13.2131Z" fill="#999999"/>
+            </Svg>}
         </TouchableOpacity>
     )
 }
@@ -19,16 +24,20 @@ export default Button = ({text}) =>{
 
 const styles = StyleSheet.create({
     button: {
-      backgroundColor: '#0E1422',
-      borderRadius : 10,
-      width : 350,
-      paddingVertical : 20,
-      alignItems: 'center',
-      marginRight : 10,
-      marginVertical: 30,
+        backgroundColor: '#0E1422',
+        borderRadius : 10,
+        width : 350,
+        paddingVertical : 20,
+        alignItems: 'center',
+        marginRight : 10,
+        marginVertical: 30,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
     },
     buttonText: {
-      color: '#FFFFFF',
-      fontSize: 14,
+        color: '#FFFFFF',
+        fontSize: 14,
+        marginRight: 10
     },
 });
