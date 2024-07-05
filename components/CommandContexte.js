03c30862679740d789e8947c6cf4f301fa910072
SarahOtmane <sarahotmane02@gmail.com>
2024-07-06 01:54:06 +0200

    update

--- components/CommandContexte.js
+++ components/CommandContexte.js
@@ -2,7 +2,10 @@ import React, { createContext, useState } from 'react';
 export const CommandContext = createContext();
 
 export const CommandProvider = ({ children }) => {
-    const [command, setCommand] = useState({});
+    const [command, setCommand] = useState({
+        specialite: '',
+        adresse: ''
+    });
 
     return (
         <CommandContext.Provider value={{ command, setCommand }}>
