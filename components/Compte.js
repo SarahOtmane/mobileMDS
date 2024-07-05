ff352eff448fe783021e8f514e5268839ffc8873
SarahOtmane <sarahotmane02@gmail.com>
2024-07-05 23:58:58 +0200

    header, image + text

--- components/Compte.js
+++ components/Compte.js
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native"
+import { View, Text, Image, ScrollView } from "react-native";
+
+import Header from "./sections/Header";
+
+import compteStyle from "../styles/compteStyle";
+import Style from '../Style';
 
 
 const Compte = () =>{
     return(
-        <View>
-            <Text>Sarah</Text>
-        </View>
+        <ScrollView style={compteStyle.container}>
+            <View>
+                <Header />
+                <Image style={compteStyle.image} source={require('../assets/compte.png')} />
+                <Text style={[Style.h6, Style.m_R10, Style.m_T20]}>Identifiez vous pour suivre vos commandes, valider votre commande.</Text>
+            </View>
+        </ScrollView>
     )
 }
 
