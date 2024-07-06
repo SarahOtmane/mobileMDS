db29161b61b60449d7ae4a10e5d617b24c480f18
SarahOtmane <sarahotmane02@gmail.com>
2024-07-06 11:04:53 +0200

    update onPress function

--- components/sections/RepaForm.js
+++ components/sections/RepaForm.js
@@ -1,28 +1,41 @@
 import React, { useState } from 'react';
-
 import Input from './Input';
 import Button from './Button';
 
-
-
-export default RepaForm = ({command, setCommand}) =>{
-    const [form, setAddress] = useState({
-        address : false
+const RepaForm = ({ command, setCommand }) => {
+    const [form, setForm] = useState({
+        address: false
     });
 
-    return(
+    const suiteForm = () => {
+        if (!form.address ) setForm((prevForm) => ({ ...prevForm, address: true }));
+    };
+
+    return (
         <>
-            {!form.address &&
-                <Input label = 'Specialité, ...' placeholder='Specialité'
-                 command={command} setCommand={setCommand} recherche='true' />
-            }
+            {!form.address && (
+                <Input 
+                    label='Specialité' 
+                    placeholder='Specialité'
+                    command={command} 
+                    setCommand={setCommand} 
+                    recherche={true} 
+                />
+            )}
 
-            {form.address &&
-                <Input label='Où? Adresse, Ville, ...' placeholder='Lieu'
-                command={command} setCommand={setCommand} recherche='true' />
-            }
+            {form.address && (
+                <Input 
+                    label='Où? Adresse, Ville, ...' 
+                    placeholder='Lieu'
+                    command={command} 
+                    setCommand={setCommand} 
+                    recherche={true} 
+                />
+            )}
 
-            <Button text='Suivant' arrow={false} bottom={true} />
+            <Button onPress={suiteForm} text='Suivant' arrow={false} bottom={true} />
         </>
-    )
-}
+    );
+};
+
+export default RepaForm;
