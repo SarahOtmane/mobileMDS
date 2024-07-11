4ca7f4a6616bb2cd67c2aa8a12487c8b9a1c72ea
SarahOtmane <sarahotmane02@gmail.com>
2024-07-11 16:26:01 +0200

    composant pour l inscription

---
+++ components/screen/user/Inscription.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Text, Image, ScrollView, View} from 'react-native';
+import HeaderScreen from '../HeaderScreen';
+
+import compteStyle from '../../../styles/compteStyle';
+import Button from '../../sections/Button';
+import Style from '../../../Style';
+
+export default Inscription = ({ navigation }) => {
+    const [donnee, setDonnee] = useState({
+        email: '',
+        password: '',
+        errors: false
+    });
+
+    return (
+        <ScrollView>
+            <HeaderScreen titre='Créer un compte' navigation={navigation} />
+
+            <View style={[compteStyle.seConnecter]}>
+                <Input 
+                    label='Email'
+                    placeholder='votre e-mail'
+                    command={donnee} 
+                    setCommand={setDonnee} 
+                    recherche={false} 
+                    error={donnee.errors}
+                />
+                <Input 
+                    label='Mot de passe'
+                    placeholder='votre mot de passe'
+                    command={donnee} 
+                    setCommand={setDonnee} 
+                    recherche={false} 
+                    error={donnee.errors}
+                />
+                <Text style={[Style.textCenter]} >Mot de passe oublié ?</Text>
+            </View>
+
+            <View>
+                <Button 
+                    text='Se connecter' 
+                    arrow={true} 
+                    bottom={false} 
+                    onPress={() => navigation.navigate('Connexion')} 
+                    classStyle='seConnecterButton'
+                />
+                <Text style={[Style.textCenter]} >Vous avez déjà un compte? S’inscrirer ?</Text>
+            </View>
+        </ScrollView>
+    );
+}
