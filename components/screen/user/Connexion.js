1162dc347996a0d91fafb7e0e6ded3f49b6c77c9
SarahOtmane <sarahotmane02@gmail.com>
2024-07-11 12:40:53 +0200

    add input

--- components/screen/user/Connexion.js
+++ components/screen/user/Connexion.js
@@ -1,11 +1,40 @@
-import React from 'react';
-import { Text, Image, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { Text, Image, ScrollView, View } from 'react-native';
+import HeaderScreen from '../HeaderScreen';
 
 
 export default Connexion = ({navigation}) =>{
+    const [donnee, setDonnee] = useState({
+        email: '',
+        password: '',
+        errors: false
+    })
+
     return(
         <ScrollView>
-            <Text>Connexion</Text>
+            <HeaderScreen titre='Se connecter' navigation={navigation} />
+
+            <View>
+                <Input 
+                    label='Email'
+                    placeholder='votre e-mail'
+                    command={donnee} 
+                    setCommand={setDonnee} 
+                    recherche={false} 
+                    error={donnee.errors}
+                />
+    
+                <Input 
+                    label='Mot de passe'
+                    placeholder='votre mot de passe'
+                    command={donnee} 
+                    setCommand={setDonnee} 
+                    recherche={false} 
+                    error={donnee.errors}
+                />
+    
+                <Text>Mot de passe oublié ?</Text>
+            </View>
         </ScrollView>
     )
 }
